ac195fbd0e30f75f66a6a2bd5fe5608d64c7b5b8
Aluoní <105504582+AluoniLopes@users.noreply.github.com>
2025-02-13 19:27:57 -0300

    optional description

--- divulgar/models.py
+++ divulgar/models.py
@@ -23,7 +23,7 @@ class Pet(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     foto = models.ImageField(upload_to="fotos_pets")
     nome = models.CharField(max_length=100)
-    descricao = models.TextField()
+    descricao = models.TextField(blank=True)
     estado = models.CharField(max_length=50)
     cidade = models.CharField(max_length=50)
     telefone = models.CharField(max_length=50)
